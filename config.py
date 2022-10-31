74dfef084f27a70c6e99671ce86f1631637d184c
Shahen <shahenmalsha17@gmail.com>
2022-11-01 03:58:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-private_k_default = "e15bd378c65ba5bd25e046c2ffc3fe959c0055902177ef575aa19120fb8adbec" #default address private key
-address_default = "0xC54Be86282B4943D14F84d49f86FBcC33930954f" #default public address
-data_seed = "https://data-seed-prebsc-1-s1.binance.org:8545/" #https://data-seed-prebsc-1-s1.binance.org:8545/ (testnet_binance)
-                                                              #https://bsc-dataseed.binance.org/ (mainnet_binance)
-                                                              # for eth rpc use infura api
+private_k_default = "" #default address private key
+address_default = "" #default public address
+data_seed = "" #https://data-seed-prebsc-1-s1.binance.org:8545/ (testnet_binance)
+               #https://bsc-dataseed.binance.org/ (mainnet_binance)
+               # for eth rpc use infura api
 
